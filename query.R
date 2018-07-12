5a90eb4a9465c7bf9c349087173bf568482918a0
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-07-12 19:59:18 -0400

    Add additional models

--- query.R
+++ query.R
@@ -1,21 +1,37 @@
 # Request query
 query <- readline(prompt = "Enter a product name: ")
+model <- readline(prompt = "Enter a predictive model (nb/dt/knn): ")
+predict.query(query, model)
 
-# Split query into seperate elements
-query <- strsplit(query, " ")
 
-# Stem query elements
-query.v <- character()
-for (i in query) {
-  i <- wordStem(i)  
-  query.v <- c(query.v, i)
+predict.query <- function(query, model) {
+  # Split query into seperate elements
+  query <- strsplit(query, " ")
+  
+  # Stem query elements
+  query.v <- character()
+  for (i in query) {
+    i <- wordStem(i)  
+    query.v <- c(query.v, i)
+  }
+  
+  # Convert query to document term matrix
+  query.df <- dtm.train.df[0,]  # Requires same attribute length as training set
+  query.df[1, 1:(ncol(query.df)-1)] <- 0
+  query.df[1, names(query.df) %in% query.v] <- 1
+  
+  
+  # Return predicted category according to model
+  if (model == "dt") {
+    result <- predict(dt.model, query.df[,names(query.df) != "category"], type="class")
+  } else if (model == "nb") {
+    result <- predict(nb.model, query.df[,names(query.df) != "category"])
+  } else if (model == "knn") {
+    result <- knn(dtm.train.df[,names(dtm.train.df) != "category"],
+                  query.df[,names(query.df) != "category"], 
+                  dtm.train.df$category, k=1) # Test different k
+  }
+  print(noquote(paste("Recommended category: ", result)))
+  print(noquote("Terms in vocabulary:"))
+  print(colnames(query.df)[which(query.df == 1)])
 }
-
-# Convert query to document term matrix
-query.df <- data.frame(rep(1, len = length(query.v)), row.names = query.v)
-query.df <- as.data.frame(t(query.df))
-query.df[, 1:length(query.df)] <- lapply(query.df[, 1:length(query.df)], factor, levels = c("0", "1"))
-
-# Return predicted category
-result <- predict(nb.model, query.df)
-print(paste("Recommended category: ", result))
