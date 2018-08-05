36b3dbf1add9b681a2d13513f3e120d3f17196cb
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-05 00:26:39 -0400

    Initial commit

---
+++ functions.R
@@ -0,0 +1,18 @@
+#### Functions ####
+
+# install.packages("ggplot2")
+library(ggplot2)
+
+# Plot confusion matrix
+plot.cm <- function(cm, title = NULL){
+  p <-
+    ggplot(data = as.data.frame(cm), aes(x = Predicted, y = Actual)) +
+    geom_tile(aes(fill = log(Freq)), colour = "white") +
+    scale_fill_gradient(low = "white", high = "steelblue") +
+    geom_text(aes(x = Predicted, y = Actual, label = Freq), size = 3) +
+    theme(legend.position = "none") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    ggtitle(title)
+  return(p)
+}
