f8417d083def368e4954fecc7468d9591682bfa9
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-05 14:40:56 -0400

    Change background colour of 0 values to white

--- functions.R
+++ functions.R
@@ -8,7 +8,7 @@ plot.cm <- function(cm, title = NULL){
   p <-
     ggplot(data = as.data.frame(cm), aes(x = Predicted, y = Actual)) +
     geom_tile(aes(fill = log(Freq)), colour = "white") +
-    scale_fill_gradient(low = "white", high = "steelblue") +
+    scale_fill_gradient(low = "white", high = "steelblue", na.value="white") +
     geom_text(aes(x = Predicted, y = Actual, label = Freq), size = 3) +
     theme(legend.position = "none") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
