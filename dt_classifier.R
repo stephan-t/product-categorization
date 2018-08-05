9f1a75552ad97268c93da783ec588d16e000a6e2
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-05 00:29:09 -0400

    Add confusion matrix plot

--- dt_classifier.R
+++ dt_classifier.R
@@ -3,16 +3,17 @@
 # install.packages("rpart")
 library(rpart)
 source("preprocessor.R")
+source("functions.R")
 
 # Preprocess data using specified sparsity
-# data <- preprocess(0.99999)
+data <- preprocess(0.99999)
 
 
 #### Classification ####
 
 # Set training sample size
 set.seed(100)
-idx <- sample(1:nrow(data), floor(nrow(data) * .8))
+idx <- sample(1:nrow(data), floor(nrow(data) * (2/3)))
 
 # Create training and testing sets
 data.train <- data[idx,]
@@ -20,7 +21,7 @@ data.test <- data[-idx,]
 
 # Create model
 time.start <- Sys.time()
-dt.ctrl <- rpart.control(cp = 0.001, minsplit = 1)
+dt.ctrl <- rpart.control(cp = 0.001, minsplit = 20)
 dt.model <- rpart(category ~ ., data.train[, -1], method="class", 
                   parms = list(split="information"), control = dt.ctrl)
 (time.end <- Sys.time() - time.start)
@@ -35,17 +36,18 @@ dt.pred <- predict(dt.model, data.test[, -c(1, ncol(data.test))], type="class")
 
 # Create data frame of predicted and actual class labels
 dt.pred.df <- data.frame(name=data.test[, 1])
-dt.pred.df <- cbind(dt.pred.df, actual=as.character(data.test$category), stringsAsFactors = FALSE)
-dt.pred.df <- cbind(dt.pred.df, predict=as.character(dt.pred), stringsAsFactors = FALSE)
+dt.pred.df <- cbind(dt.pred.df, actual=data.test$category)
+dt.pred.df <- cbind(dt.pred.df, predict=dt.pred)
 
 # Create data frame of misclassified objects
 dt.misclass <- dt.pred.df[which(dt.pred.df$predict != dt.pred.df$actual),]
 
-# Show confusion matrix
-dt.cm <- table(dt.pred.df$actual, factor(dt.pred), dnn=c("Actual","Predicted"))
+# Create confusion matrix
+dt.cm <- table(dt.pred.df$actual, dt.pred.df$predict, dnn=c("Actual","Predicted"))
+plot.cm(dt.cm, title = "Confusion Matrix of Decision Tree")
 
 # Calculate accuracy
-nrow(dt.pred.df[which(dt.pred.df$predict == dt.pred.df$actual),]) / nrow(dt.pred.df)
+cat("Accuracy:", sum(diag(dt.cm))/sum(dt.cm))
 
 
 # Build final model using all data
