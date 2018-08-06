7ec281aa86ca0e76d96abe82aec7d66d786e0b8c
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-06 04:55:56 -0400

    Change prediction to use full data set

--- query.R
+++ query.R
@@ -1,3 +1,5 @@
+#### Query Predicton ####
+
 library(SnowballC)
 library(e1071)
 library(rpart)
@@ -11,28 +13,28 @@ predict.query <- function(query, model) {
   # Stem query elements
   query.v <- character()
   for (i in query) {
-    i <- wordStem(i)  
+    i <- wordStem(i)
     query.v <- c(query.v, i)
   }
   
   # Convert query to document term matrix
-  query.df <- dtm.train.df[0,]  # Requires same attribute length as training set
-  query.df[1, 1:(ncol(query.df)-1)] <- 0
+  query.df <- data[0,]  # Requires same number of columns as data set
+  query.df[1, 2:(ncol(query.df)-1)] <- 0
   query.df[1, names(query.df) %in% query.v] <- 1
   
   
   # Return predicted category according to model
   if (model == "dt") {
-    result <- predict(dt.model, query.df[,names(query.df) != "category"], type="class")
+    result <- predict(dt.model, query.df[, -c(1, ncol(query.df))], type="class")
   } else if (model == "nb") {
-    result <- predict(nb.model, query.df[,names(query.df) != "category"])
+    result <- predict(nb.model, query.df[, -c(1, ncol(query.df))])
   } else if (model == "knn") {
     k <- as.numeric(readline("Enter number of nearest neighbours (k): "))
-    result <- knn(dtm.train.df[,names(dtm.train.df) != "category"],
-                  query.df[,names(query.df) != "category"], 
-                  dtm.train.df$category, k=k)
+    result <- knn(data[, -c(1, ncol(data))],
+                  query.df[, -c(1, ncol(query.df))], 
+                  data$category, k=k)
   }
-  cat("Recommended category: ", as.character(result))
+  cat("Recommended category:", as.character(result))
   cat("\n")
   cat("Terms in vocabulary:", colnames(query.df)[which(query.df == 1)])
 }
