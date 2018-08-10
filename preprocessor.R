03b201db3a568b8fa55a41ddb46138b4e755bc0c
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-10 02:03:40 -0400

    Use same preprocess function for query

--- preprocessor.R
+++ preprocessor.R
@@ -5,35 +5,42 @@
 # install.packages("RWeka")
 library(tm)        # Text mining
 library(SnowballC) # Stemming
-# library(RWeka)     # n-grams
+# library(RWeka)   # n-grams
 
-# Preprocess function to run from each classifier
-preprocess <- function(sparsity) {
-  time.start <- Sys.time()
-  
-  # Import data
-  data <- read.csv("data/flipkart.csv")
-  
-  # Create data frame with name and category columns
-  data <- data[, 4:5]
-  colnames(data) <- c("name","category")
-  
-  # Remove duplicates
-  data <- unique(data)
-  
-  # Extract top category
-  data$category <- gsub('(\\["| >>.*)', "", data$category)
-  data$category <- as.factor(data$category)
-  
-  # Remove unknown categories
-  data <- data[(grep('.*"]', data$category, invert = TRUE)),]
-  rownames(data) <- 1:nrow(data)
 
+# Preprocess data set or query
+preprocess <- function(sparsity = NULL, query = NULL) {
+  # time.start <- Sys.time()
   
-  #### Text Processing ####
+  if (is.null(query)) {
+    # Import data
+    data <- read.csv("data/flipkart.csv")
+    
+    # Create data frame with name and category columns
+    data <- data[, 4:5]
+    colnames(data) <- c("name","category")
+    
+    # Remove duplicates
+    data <- unique(data)
+    
+    
+    #### Text Processing ####
+    
+    # Extract top category
+    data$category <- gsub('(\\["| >>.*)', "", data$category)
+    data$category <- as.factor(data$category)
+    
+    # Remove unknown categories
+    data <- data[(grep('.*"]', data$category, invert = TRUE)),]
+    rownames(data) <- 1:nrow(data)
+  }
   
-  # Create corpus
-  data.corp <- VCorpus(VectorSource(data$name))
+  # Create corpus from data set or query
+  if (is.null(query)) {
+    data.corp <- VCorpus(VectorSource(data$name))
+  } else {
+    data.corp <- VCorpus(VectorSource(query))
+  }
   
   # Function to count words once
   uniqueWords <- function(word) {
@@ -47,9 +54,10 @@ preprocess <- function(sparsity) {
   data.corp <- tm_map(data.corp, stripWhitespace)
   data.corp <- tm_map(data.corp, removeWords, stopwords("english"))
   data.corp <- tm_map(data.corp, stemDocument, language = "english")
-  # data.corp <- tm_map(data.corp, removeWords, c("black","blue","white","yellow","men",
-  #                                     "women","boy","girl"))
   data.corp <- tm_map(data.corp, content_transformer(uniqueWords))
+  
+  
+  #### Feature Extraction ####
 
   # Create document term matrix with 2-gram features
   # BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
@@ -59,19 +67,30 @@ preprocess <- function(sparsity) {
   data.dtm <- DocumentTermMatrix(data.corp)
   
   # Reduce size of DTM by removing less frequent terms
-  data.dtm <- removeSparseTerms(data.dtm, sparsity)
+  if (is.null(query)) {
+    data.dtm <- removeSparseTerms(data.dtm, sparsity)
+  }
   
-  # Convert DTM to data frame and append product names and class
+  # Convert DTM to data frame
   data.df <- data.frame(as.matrix(data.dtm))
-  data.df <- cbind(name = data$name, data.df)
-  data.df <- cbind(data.df, category = data$category)
+  if (is.null(query)) {
+    # Append product names and classes
+    data.df <- cbind(name = data$name, data.df)
+    data.df <- cbind(data.df, category = data$category)
+  }
   
-  # Change term columns from numeric to factor
-  data.df[, -c(1, ncol(data.df))] <- lapply(data.df[, -c(1, ncol(data.df))], factor)
+  # Change term columns to binary factor
+  if (is.null(query)) {
+    data.df[, -c(1, ncol(data.df))] <- lapply(data.df[, -c(1, ncol(data.df))], factor)
+  } else {
+    data.df[, 1:ncol(data.df)] <- lapply(data.df[, 1:ncol(data.df)] , factor, levels = c("0","1"))
+  }
 
   # Update class label factor levels
-  data.df$category <- factor(data.df$category)
+  if (is.null(query)) {
+    data.df$category <- factor(data.df$category)
+  }
   
-  print(time.end <- Sys.time() - time.start)
+  # print(time.end <- Sys.time() - time.start)
   return(data.df)
 }
--- query.R
+++ query.R
@@ -1,46 +1,54 @@
-#### Query Predicton ####
+#### User Query Predicton ####
 
-library(SnowballC)
 library(e1071)
 library(rpart)
 library(class)
+source("preprocessor.R")
 
 
-predict.query <- function(query, model) {
-  # Split query into seperate elements
-  query <- strsplit(query, " ")
+query.predict <- function(query, model) {
+  # Preprocess query terms
+  query.df <- preprocess(query = query)
   
-  # Stem query elements
-  query.v <- character()
-  for (i in query) {
-    i <- wordStem(i)
-    query.v <- c(query.v, i)
-  }
+  # Merge query with vocabulary to match dimensions
+  data.vocab <- data[0, -c(1, ncol(data))]
+  data.vocab[1,] <- 0
+  query.df <- merge(data.vocab, query.df[names(query.df) %in% names(data.vocab)], 
+                    all = TRUE, all.x = FALSE)
+  query.df[, is.na(query.df)] <- 0
   
-  # Convert query to document term matrix
-  query.df <- data[0,]  # Requires same number of columns as data set
-  query.df[1, 2:(ncol(query.df)-1)] <- 0
-  query.df[1, names(query.df) %in% query.v] <- 1
+  # Change term columns to binary factor
+  query.df[,1:ncol(query.df)] <- lapply(query.df[,1:ncol(query.df)], factor, levels = c("0","1"))
   
+  # Order column names alphabetically
+  query.df <- query.df[, order(names(query.df))]
   
-  # Return predicted category according to model
+  # Predict category according to model
   if (model == "dt") {
-    result <- predict(dt.model, query.df[, -c(1, ncol(query.df))], type="class")
+    result <- predict(dt.model, query.df, type="class")
   } else if (model == "nb") {
-    result <- predict(nb.model, query.df[, -c(1, ncol(query.df))])
+    result <- predict(nb.model, query.df)
   } else if (model == "knn") {
     k <- as.numeric(readline("Enter number of nearest neighbours (k): "))
-    result <- knn(data[, -c(1, ncol(data))],
-                  query.df[, -c(1, ncol(query.df))], 
-                  data$category, k=k)
+    result <- knn(data[, -c(1, ncol(data))], query.df, data$category, k = k)
   }
+  
+  # Find matched query terms in vocabulary
+  vocab.term <- colnames(query.df)[colnames(query.df) %in% colnames(data) & query.df[1,] == 1]
+  query.term <- strsplit(query.in, " ")
+  
+  # Print results
   cat("Recommended category:", as.character(result))
   cat("\n")
-  cat("Terms in vocabulary:", colnames(query.df)[which(query.df == 1)])
+  cat("Terms in vocabulary:", vocab.term)
+  cat("\n")
+  cat("Term match:", (length(vocab.term) / length(query.term[[1]])))
 }
 
 
 # Request query
-query <- readline(prompt = "Enter product name: ")
+query.in <- readline(prompt = "Enter product name: ")
 model <- readline(prompt = "Enter predictive model (nb/dt/knn): ")
-predict.query(query, model)
+
+# Predict category from query
+query.predict(query.in, model)
