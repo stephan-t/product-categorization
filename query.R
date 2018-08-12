63d0642d5d932b83d8d7aa8911ea19b27972277a
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-08-12 13:37:10 -0400

    Add printing of vocabulary size and sorting by model score

--- query.R
+++ query.R
@@ -52,25 +52,13 @@ query.predict <- function(query) {
   knn.query.pred <- as.character(knn(knn.data[, -c(1, ncol(knn.data))], knn.query.df, 
                                      knn.data$category, k = 3))
   
-  # Create data frame of all model predictions
+  # Create data frame of model predictions
   query.pred.df <- data.frame(row.names = c("Decision Tree", "Naive Bayes", "k-NN"), 
                               Category = c(dt.query.pred, nb.query.pred, knn.query.pred), 
                               stringsAsFactors = FALSE)
   
   
   #### Evaluation ####
-  
-  # Calculate percent of matched vocabulary terms in query
-  dt.vocab.term <- colnames(dt.query.df)[colnames(dt.query.df) %in% colnames(dt.data) 
-                                         & dt.query.df[1,] == 1]
-  nb.vocab.term <- colnames(nb.query.df)[colnames(nb.query.df) %in% colnames(nb.data) 
-                                         & nb.query.df[1,] == 1]
-  knn.vocab.term <- colnames(knn.query.df)[colnames(knn.query.df) %in% colnames(knn.data) 
-                                         & knn.query.df[1,] == 1]
-  query.term <- strsplit(query.in, " ")
-  query.pred.df$Match <- c(length(dt.vocab.term) / length(query.term[[1]]),
-                           length(nb.vocab.term) / length(query.term[[1]]),
-                           length(knn.vocab.term) / length(query.term[[1]]))
     
   # Calculate evaluation metrics
   dt.precis <- dt.cm[dt.query.pred, dt.query.pred] / sum(dt.cm[,dt.query.pred])
@@ -84,19 +72,39 @@ query.predict <- function(query) {
   query.pred.df <- cbind(query.pred.df, Precision = c(dt.precis, nb.precis, knn.precis))
   query.pred.df <- cbind(query.pred.df, Recall = c(dt.recall, nb.recall, knn.recall))
   
+  # Calculate percent of matched vocabulary terms in query
+  dt.vocab.term <- colnames(dt.query.df)[colnames(dt.query.df) %in% colnames(dt.data) 
+                                         & dt.query.df[1,] == 1]
+  nb.vocab.term <- colnames(nb.query.df)[colnames(nb.query.df) %in% colnames(nb.data) 
+                                         & nb.query.df[1,] == 1]
+  knn.vocab.term <- colnames(knn.query.df)[colnames(knn.query.df) %in% colnames(knn.data) 
+                                           & knn.query.df[1,] == 1]
+  query.term <- strsplit(query.in, " ")
+  query.pred.df$Match <- c(length(dt.vocab.term) / length(query.term[[1]]),
+                           length(nb.vocab.term) / length(query.term[[1]]),
+                           length(knn.vocab.term) / length(query.term[[1]]))
+  
+  # Create data frame of model vocabularies
+  query.vocab.df <- data.frame(row.names = c("Decision Tree", "Naive Bayes", "k-NN"),
+                               Size = c(length(dt.data)-2, length(nb.data)-2, length(knn.data)-2),
+                               Match = c(paste0("{", paste(dt.vocab.term, collapse = ", "), "}"), 
+                                         paste0("{", paste(nb.vocab.term, collapse = ", "), "}"),
+                                         paste0("{", paste(knn.vocab.term, collapse = ", "), "}")), 
+                               stringsAsFactors = FALSE)
+  
   # Calculate average score
   query.pred.df$Score <- rowMeans(query.pred.df[,2:5])
+  query.vocab.df$Score <- rowMeans(query.pred.df[,2:5])
   
   # Sort by highest average score
   query.pred.df <- query.pred.df[order(query.pred.df$Score, decreasing = TRUE),]
+  query.vocab.df <- query.vocab.df[order(query.vocab.df$Score, decreasing = TRUE),]
  
   # Print results
-  cat("Recommended categories:", "\n")
-  print(query.pred.df)
-  cat("\n")
-  cat("Decision tree vocabulary:", "\t", dt.vocab.term, "\n")
-  cat("Naive Bayes vocabulary:", "\t", nb.vocab.term, "\n")
-  cat("k-NN vocabulary:", "\t\t", knn.vocab.term, "\n")
+  cat("\nRecommended Categories:\n")
+  print.data.frame(query.pred.df, digits = 3)
+  cat("\nClassifier Vocabularies:\n")
+  print.data.frame(query.vocab.df[, -3])
 }
 
 
